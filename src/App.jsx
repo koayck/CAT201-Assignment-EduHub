1325ccf3536aeec7d26b1b76533ffb9ce0b8944f
koayck <116639605+koayck@users.noreply.github.com>
2023-12-17 08:39:06 +0800

    🎉 set up

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import NavBar from "./components/NavBar";
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <HeroSection />
+    </>
+  );
+}
+
+export default App;
