ed3e11ae3d96acc69a7059754bde0db99f1b439a
koayck <116639605+koayck@users.noreply.github.com>
2023-12-18 17:23:28 +0800

    delete pr workflow and make navbar menu bigger

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -47,7 +47,7 @@ const NavBar = () => {
               </div>
               <ul
                 tabIndex="0"
-                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                className="menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
               >
                 <li>
                   <a href="#">Home</a>
