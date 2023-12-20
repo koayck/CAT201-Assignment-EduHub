f2329bc94d483221f6009b593757dbde0489b81f
Jisi-A <7216lyc@gmail.com>
2023-12-20 19:54:25 +0800

    ✨feat: create team component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Team from "./components/Team";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Team />
       <Footer />
     </>
   );
