069fcfff7bb8ee6250350b576bf3cbed4f34bbbe
JohnasLeee <johnas@DESKTOP-BK7JFP2.localdomain>
2023-12-20 17:37:03 +0800

    added statistics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import Statistics from "./components/Statistics";
 
 function App() {
   return (
     <>
       <NavBar />
+      <Statistics />
       <Footer />
     </>
   );
