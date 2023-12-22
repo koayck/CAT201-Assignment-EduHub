3749ff15d252828172e8d8b2b92433f13b50923f
koayck <116639605+koayck@users.noreply.github.com>
2023-12-22 20:48:00 +0800

    ✨ feat: complete hero section and hero card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
+import HeroCard from "./components/HeroCard";
 
 function App() {
   return (
     <>
       <NavBar />
+      <HeroSection />
+      <HeroCard />
       <Footer />
     </>
   );
